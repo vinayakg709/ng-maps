ea1ec23a526ac654f72c0025f9b75dcd80394e43
vinayakg7091999 <vinayakg709@gmail.com>
2019-11-06 23:10:18 +0530

    added google map api

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {} from 'googlemaps';
+import { ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'ng-maps';
+  @ViewChild('map') mapElement: any;
+  map: google.maps.Map;
+
+ngOnInit(): void {
+  const mapProperties = {
+       center: new google.maps.LatLng(35.2271, -80.8431),
+       zoom: 15,
+       mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  this.map = new google.maps.Map(this.mapElement.nativeElement,    mapProperties);
+}
+}
